4e8008cf1e50c8a2558297351445afdb5cf68085
salmon <salmon@hornn.es>
2020-07-10 13:54:36 +0200

    added bulma and navbar

--- test-app-with-routing/src/app/app-routing.module.ts
+++ test-app-with-routing/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ test-app-with-routing/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  name: 'dashboard';
+
+  constructor(
+    private route: ActivatedRoute, 
+  ) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params=> {
+      this.name = params['name'];
+    })
+  }
+
+}
